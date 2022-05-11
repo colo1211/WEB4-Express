0bfbf854ec2eee849f1596927fec988b9d187686
colo1211 <colo1211@naver.com>
2022-05-11 12:15:46 +0900

    로그인 구현 passport

--- TODOAPP/server.js
+++ TODOAPP/server.js
@@ -5,10 +5,11 @@ app.set('view engine', 'ejs');
 app.use('/public', express.static('public')); 
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
+require('dotenv').config();
 
 var db; // db 를 담을 변수
 MongoClient.connect(
-  'mongodb+srv://admin:1234@cluster0.skcs6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  process.env.DB_URL,
   function (err, client) {
     if (err) {
       return console.log(err);
@@ -19,7 +20,7 @@ MongoClient.connect(
     //     console.log('저장완료');
     // });
 
-    app.listen(8080, function () {
+    app.listen(process.env.PORT, function () {
       console.log('8080 가동 중');
     });
   }
@@ -27,13 +28,88 @@ MongoClient.connect(
 
 app.use(express.urlencoded({ extended: true }));
 
+const passport = require('passport'); 
+const LocalStrategy = require('passport-local').Strategy; // 로컬전략
+const session = require('express-session'); 
+
+// 미들웨어
+app.use(session({secret:'비밀코드', resave : true, saveUninitialized : false})); 
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+app.get('/login', function(req,res){
+  res.render('login.ejs'); 
+})
+
+// 폼데이터로 로그인 정보를 보내면, 미들웨어(passport.authenticate('local',{}))로 검사
+// 맞다면 '/' 으로 리다이렉트
+// 틀리다면 '/fail' 로 리다이렉트
+app.post('/login', passport.authenticate('local',{
+  failureRedirect : '/fail',
+  successRedirect : '/',
+}));
+
+
+// 미들웨어
+function checkLogin(req, res, next){
+  console.log('checkLogIn', req.user); 
+  // req.user는 로그인 후, 세션이 있으면 항상 req.user가 존재한다.
+  if(req.user){
+    console.log('로그인O'); 
+    next(); // 다음으로 통과
+  }else{
+    console.log('로그인X'); 
+    res.redirect('/login'); 
+  }
+}
+
+// passport의 미들웨어, id와 pw를 받아서 db에 조회하는 역할
+passport.use(new LocalStrategy({
+  usernameField: 'id', // 폼데이터에서 입력한 id 
+  passwordField: 'pw', // 폼데이터에서 입력한 pw
+  session: true, // 세션아이디를 만들어서 저장 여부 
+  passReqToCallback: false, // id, pw 이외의 다른 정보가 존재하는지? -> 만약 존재한다면, 콜백함수의 첫 인자로 객체형태(req)로 받을 수 있음
+}, function (id, pw, done) {
+  console.log('passport에서 id/pw 조회',id, pw); // 유저가 입력한 id와 pw 정보
+  // login 콜렉션에 id 정보가 존재하는지 조회
+  db.collection('login').findOne({ id: id }, function (err, result) {
+    if (err) return done(err)
+    // id를 조회해서 result 가 없으면
+    if (!result) return done(null, false, { message: '존재하지않는 아이디' })
+    // result 가 있으면
+    if (pw == result.pw) {
+      return done(null, result) // 여기서 result 는 serializeUser의 user 값으로 전달
+    } else {
+      return done(null, false, { message: '비번틀림' })
+    }
+  })
+}));
+
+// serializeUser 
+// 로그인 했을 때, 최초로 한번 호출되는 함수
+// 세션아이디를 쿠키에 자동으로 저장하는 함수
+passport.serializeUser(function(user, done){
+  console.log('serializeUser 들어옴', user.id);
+  done(null,user.id); // serializeUser에서 done(null, user.id)를 호출하면, 세션에 id 값이 저장
+})
+
+// deserializeUser 
+// 로그인에 성공하고, 페이지를 방문할 때마다 호출
+//  deserializeUser가 호출되면 request.user 라는 객체가 생성되므로 유저에 대한 정보를 손쉽게 사용할 수 있음
+passport.deserializeUser(function(아이디,done){
+  console.log('deserializeUser 들어옴',아이디);
+  db.collection('login').findOne({ id : 아이디}, function(err,result){
+    console.log(result); 
+    done(null, result);
+  })
+})
+
 app.get('/', function (req, res) {
   // res.sendFile(__dirname + '/index.html');
   res.render('index.ejs'); 
 });
 
-app.get('/write', function (req, res) {
-  // res.sendFile(__dirname + '/write.html'); 
+app.get('/write', checkLogin, function (req, res) {
   res.render('write.ejs'); 
 });
 
@@ -65,8 +141,7 @@ app.post('/add', function (req, res) {
         })
       }
     );
-  })
-  
+  });
 });
 
 app.get('/list', function (req, res) {
@@ -117,3 +192,9 @@ app.put('/edit', function(req,res){
     res.redirect(`/detail/${req.body.id}`)
   })
 });
+
+app.get('/mypage', checkLogin, function(req,res){
+  console.log(req.user); 
+  res.render('mypage.ejs', {data : req.user}); 
+});
+
